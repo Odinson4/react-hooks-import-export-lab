f408170d3de37add1c2b58aea584e58643acdc07
Dylan Abbott <dylanabbott33@yahoo.com>
2023-02-20 16:52:16 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import About from "./About";
+// import { username, image, city } from "../data/user.js";
 
 function App() {
   return (
